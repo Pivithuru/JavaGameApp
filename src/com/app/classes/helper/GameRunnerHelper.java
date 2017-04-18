240085ae953268bada691a9fa2f232caff9b1b2d
pivithurul <pivithuru.lakshan@pagero.com>
2017-04-18 12:43:55 +0530

    change setCharacterDetails method to use FighterFactory class

--- src/com/app/classes/helper/GameRunnerHelper.java
+++ src/com/app/classes/helper/GameRunnerHelper.java
@@ -1,15 +1,12 @@
 package com.app.classes.helper;
 
 import com.app.classes.abstracts.Fighter;
-import com.app.classes.bean.FireSoldier;
-import com.app.classes.bean.Prince;
-import com.app.classes.bean.Princess;
+import com.app.classes.factory.FighterFactory;
 import com.app.enums.CharacterEnum;
 
 import java.util.Random;
 
 import static com.app.classes.helper.PrinterHelper.*;
-import static com.app.enums.CharacterEnum.*;
 
 /**
  * Created by pivithuru on 4/3/17 3:13 PM.
@@ -17,26 +14,16 @@ import static com.app.enums.CharacterEnum.*;
 public class GameRunnerHelper {
 
     public static Fighter setCharacterDetails(CharacterEnum characterId, String name, int age, String country) {
-        if (characterId == FIRESOLDIER) {
-            FireSoldier fireSoldier = new FireSoldier();
-            fireSoldier.setName(name);
-            fireSoldier.setAge(age);
-            fireSoldier.setCountry(country);
-            return fireSoldier;
-        } else if (characterId == PRINCE) {
-            Prince prince = new Prince();
-            prince.setName(name);
-            prince.setAge(age);
-            prince.setCountry(country);
-            return prince;
-        } else if (characterId == PRINCESS) {
-            Princess princess = new Princess();
-            princess.setName(name);
-            princess.setAge(age);
-            princess.setCountry(country);
-            return princess;
-        } else
-            return null;
+        FighterFactory fighterFactory = new FighterFactory();
+        Fighter fighter = fighterFactory.getFighter(characterId);
+
+        if (fighter != null) {
+            fighter.setName(name);
+            fighter.setAge(age);
+            fighter.setCountry(country);
+        }
+
+        return fighter;
     }
 
     public static void fightWithDragon(Fighter fighter) {
